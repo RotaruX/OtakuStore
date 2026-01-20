a05b8cf6925322d80c9a9c99173d7e49d1710c48
RotaruX <rotarualex1612@gmail.com>
2026-01-20 09:42:13 +0100

    Header casi acabado

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OtakuStore</title>
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+</head>
+<body>
+    <header>
+        <a href="index.php"><img src="assets/img/Logo.png"></a>
+
+        <div class="menu">
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="sobre_nosotros.php">Sobre Nosotros</a></li>
+                <li><a href="tienda.php">Tienda</a></li>
+                <li><a href="ferias.php">Ferias</a></li>
+            </ul>
+            <div class="usuarios">
+                <i class="fa-solid fa-user"></i>
+                <i class="fa-solid fa-cart-plus"></i>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php require_once ("includes/header.php") ?>
+
+<main>
+    <h1>Aqui va el main</h1>
+</main>
+
+<?php require_once("includes/footer.php") ?>
