5eb8c2323513d82bd12f8f8790f8680d7c87cac3
RotaruX <rotarualex1612@gmail.com>
2026-01-20 12:02:25 +0100

    Falta cosas en el footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,23 @@
     <footer>
-        Aqui va el footer
+        <div class="legal">
+            <h2>Legalidad</h2>
+            <ul>
+                <li><a href="./legal/aviso_legal.html">Aviso Legal</a></li>
+                <li><a href="./legal/politica_cookies.html">Politica de Cookies</a></li>
+                <li><a href="./legal/politica_privacidad.html">Aviso Legal</a></li>
+            </ul>
+        </div>
+
+        <div class="paginas">
+            <h2>Páginas</h2>
+            <ul>
+                <li><a href="./index.php">Inicio</a></li>
+            </ul>
+        </div>
+
+        <div class="redes">
+            <h2>Redes Sociales</h2>
+        </div>
     </footer>
 </body>
 </html>
