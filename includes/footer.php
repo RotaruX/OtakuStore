ec90f172b089ddc91be19a2864357e31d4b780d3
RotaruX <rotarualex1612@gmail.com>
2026-01-21 16:56:49 +0100

    header modificado

--- includes/footer.php
+++ includes/footer.php
@@ -12,11 +12,16 @@
             <h2>Páginas</h2>
             <ul>
                 <li><a href="./index.php">Inicio</a></li>
+                <li><a href="./tienda.php">Tienda</a></li>
+                <li><a href="./ferias.php">Ferias</a></li>
+                <li><a href="./sobre_nosotros.php">Sobre Nosotros</a></li>
             </ul>
         </div>
 
         <div class="redes">
             <h2>Redes Sociales</h2>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-linkedin"></i>
         </div>
     </footer>
 </body>
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OtakuStore</title>
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+</head>
+<body>
 <?php require_once ("includes/header.php") ?>
 
 <main>
