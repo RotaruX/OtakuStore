bc1c497ebe4d2a352cab84750d33cc8f62109542
RotaruX <rotarualex1612@gmail.com>
2026-01-23 17:26:56 +0100

    Header modificado

--- includes/footer.php
+++ includes/footer.php
@@ -1,28 +1,28 @@
     <footer>
-        <div class="legal">
+        <section class="legal">
             <h2>Legalidad</h2>
             <ul>
                 <li><a href="./legal/aviso_legal.html">Aviso Legal</a></li>
                 <li><a href="./legal/politica_cookies.html">Politica de Cookies</a></li>
                 <li><a href="./legal/politica_privacidad.html">Aviso Legal</a></li>
             </ul>
-        </div>
+        </section>
 
-        <div class="paginas">
+        <section class="paginas">
             <h2>Páginas</h2>
             <ul>
                 <li><a href="./index.php">Inicio</a></li>
                 <li><a href="./tienda.php">Tienda</a></li>
                 <li><a href="./ferias.php">Ferias</a></li>
-                <li><a href="./sobre_nosotros.php">Sobre Nosotros</a></li>
+                <li><a href="./contacto.php">Contactanos</a></li>
             </ul>
-        </div>
+        </section>
 
-        <div class="redes">
+        <section class="redes">
             <h2>Redes Sociales</h2>
             <i class="fa-brands fa-instagram"></i>
             <i class="fa-brands fa-linkedin"></i>
-        </div>
+        </section>
     </footer>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OtakuStore</title>
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+    <?php
+        $paginaActual = basename($_SERVER['PHP_SELF']);
+        if($paginaActual == 'index.php'):?>
+    <link rel="stylesheet" href="assets/css/incio.css">
+    <?php
+        elseif($paginaActual == 'tienda.php'):?>
+        <link rel="stylesheet" href="assets/css/tienda.css">
+    <?php
+        elseif($paginaActual == 'contacto.php'):?>
+    <link rel="stylesheet" href="assets/css/contacto.css">
+    <?php
+        elseif($paginaActual == 'ferias.php'):?>
+    <link rel="stylesheet" href="assets/css/ferias.css">
+    <?php endif;?>
+</head>
+<body>
     <header>
         <a href="index.php"><img src="assets/img/Logo.png"></a>
 
-        <div class="menu">
+        <section class="menu">
             <ul>
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="sobre_nosotros.php">Sobre Nosotros</a></li>
-                <li><a href="tienda.php">Tienda</a></li>
-                <li><a href="ferias.php">Ferias</a></li>
+                <li><a href="index.php" class="<?= $paginaActual == 'index.php' ? 'activo' : '' ?>">Inicio</a></li>
+                <li><a href="contacto.php" class="<?= $paginaActual == 'contacto.php' ? 'activo' : '' ?>">Contactanos</a></li>
+                <li><a href="tienda.php" class="<?= $paginaActual == 'tienda.php' ? 'activo' : '' ?>">Tienda</a></li>
+                <li><a href="ferias.php" class="<?= $paginaActual == 'ferias.php' ? 'activo' : '' ?>">Ferias</a></li>
             </ul>
-            <div class="usuarios">
+            <section class="usuarios">
                 <i class="fa-solid fa-user"></i>
                 <i class="fa-solid fa-cart-plus"></i>
-            </div>
-        </div>
+            </section>
+        </section>
     </header>
